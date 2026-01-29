e0b90f6c3fddb50b6f4c59d5669acda760a4cd4a
pedrolrm <p.lucarocha@gmail.com>
2026-01-28 23:34:26 -0300

    feat: cria função auxiliar para escrever arrays de caracteres usando ponteiros

---
+++ utils.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+char *ler_texto()
+{
+    char temp[100];
+    scanf(" %[^\n]", temp);
+
+    char *nome = (char *)malloc((strlen(temp) + 1) * sizeof(char)); // Aloca a quantidade exata de memória para o ponteiro nome
+    if (nome == NULL)
+    {
+        printf("\nFalha ao alocar memória\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(nome, temp); // Copia para nome o texto salvo na variável temporaria
+
+    return nome;
+}
