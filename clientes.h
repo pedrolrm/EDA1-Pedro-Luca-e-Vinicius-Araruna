0b7d055694dc64adfb97cbe9a978b8331f1848f4
vinicius.A <vinicius.araruna18@gmail.com>
2026-01-30 14:56:28 -0300

    feat: adiciona estrutura do cliente e carrinho + prototipo das funcoes

---
+++ clientes.h
@@ -0,0 +1,32 @@
+#ifndef CLIENTES_H
+#define CLIENTES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _carrinho {
+    int codigo_produto;
+    int quantidade;
+    struct _carrinho *prox;
+} carrinho;
+
+typedef struct _Cliente {
+    char cpf[15];
+    char *nome;
+    char email[50];
+    char telefone[20];
+    char data_nascimento [12];
+
+    carrinho *car_cabeca;
+    struct _Cliente *prox;
+} Cliente;
+
+
+void cadastrarCliente(Cliente **head);
+void listarClientes(Cliente **head);
+void editarCliente(Cliente *head);
+void removerCliente(Cliente **head);
+void liberarMemoriaTotal(Cliente **head);
+
+#endif
