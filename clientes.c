6e6d1efe43ac6bda8942a40c51e28a832725c5de
vinicius.A <vinicius.araruna18@gmail.com>
2026-01-30 15:26:05 -0300

    feat: adiciona logica de cadastro + tratamento de alocacao de memoria + lista encadeada

--- clientes.c
+++ clientes.c
@@ -12,4 +12,49 @@ Cliente *encontrar_cliente_por_cpf(Cliente *primeiro_cliente, char *cpf){
     return NULL;
 }
 
+void cadastrarCliente(Cliente **head){
+    char buffer_nome[256];
+    char cpf_temp[15];
+    printf("\n--- Novo Cadastro ---\n");
+    printf("CPF: ");
+    scanf("%[^\n]",cpf_temp);
+
+    if(encontrar_cliente_por_cpf(*head, cpf_temp) !=NULL){
+        printf("Erro: CPF ja cadastrado!");
+        return;
+    }
+    Cliente *novo_cliente =  (sizeof(Cliente));
+
+    if(!novo_cliente){
+        printf("Erro: falaha ao alocar struct\n");
+        return;
+    }
+     strcpy(novo_cliente->cpf, cpf_temp);
+     printf("Nome Completo: ");
+     scanf("%[^\n]",buffer_nome);
+
+     novo_cliente->nome = malloc((strlen(buffer_nome) + 1) *sizeof(char));
+     
+     if(novo_cliente->nome == NULL){
+        printf("Erro: falha ao alocar nome\n");
+        free(novo_cliente);
+        return;
+     }
+
+     strcpy(novo_cliente->nome, buffer_nome);
+
+     printf("Email: \n"); 
+     scanf("%[^\n]", novo_cliente->email);
+     printf("Telefone: \n");
+     scanf("%[^\n]", novo_cliente->telefone);
+     printf("Data de Nascimento: \n");
+     scanf("%[^\n]", novo_cliente->data_nascimento);
+
+     novo_cliente->car_cabeca = NULL;
+
+     novo_cliente->prox = *head;
+     *head = novo_cliente;
+
+     printf("Cliente cadastrado com sucesso!\n");
+}
 
