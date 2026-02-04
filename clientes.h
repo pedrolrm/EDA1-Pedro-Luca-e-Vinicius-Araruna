c1fd3cce382201c8f8430ceed72e67642674eebd
pedrolrm <p.lucarocha@gmail.com>
2026-02-03 22:12:48 -0300

    refactor: mudança na assinatura da função de remover do carrinho

--- clientes.h
+++ clientes.h
@@ -40,6 +40,6 @@ ItemCarrinho *cria_lista_carrinho();
 void listar_carrinho(ItemCarrinho *cabeca);
 float calcular_total_carrinho(ItemCarrinho *cabeca);
 void adiciona_ao_carrinho(Cliente *cliente_alvo, Produto *produto, int qtd);
-void remover_do_carrinho(ItemCarrinho *cabeca, int codigo_produto);
+void remover_do_carrinho(ItemCarrinho *cabeca_carrinho, Produto *cabeca_produto, int codigo_produto);
 
 #endif
